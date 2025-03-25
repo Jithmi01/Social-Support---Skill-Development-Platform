b2358c2996aa2bfb8ad7c9e2832dd4e49d752bdd
Jithmi Chathurangika <162092982+Jithmi01@users.noreply.github.com>
2025-03-25 12:39:36 +0530

    Update Financial.js

--- frontend/src/components/Financial/Financial.js
+++ frontend/src/components/Financial/Financial.js
@@ -20,6 +20,7 @@ const Financial = () => {
       alert(err.message)
     }
   }
+  
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-100">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
